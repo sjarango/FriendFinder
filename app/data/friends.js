3c020365f0f91289cc516eb0e395986d209d9881
sjarango <sjarango92@gmail.com>
2019-11-12 16:09:18 -0500

    added express server and array for data in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends answers to survey questions.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
+  
