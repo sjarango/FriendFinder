8ccaa563d05db13fd340d1a7bf59098350282fe7
Sebastian <sjarango92@gmail.com>
2020-01-24 17:31:06 -0500

    Added more friends to friends array Fixed bug and added modal with results logic in survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,7 +7,7 @@
 var friends = [
     {
         name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTcxMTQ4MzY2Nl5BMl5BanBnXkFtZTgwMzAxOTY3MjI@._V1_SY1000_CR0,0,759,1000_AL_.jpg",
         scores: [
             5,
             1,
@@ -20,6 +20,39 @@ var friends = [
             4,
             1
         ]
+    },
+    {
+        name: "Tina",
+        photo: "http://www.pajiba.com/assets_c/2018/07/tina-belcher-thumb-700x357-199206.png",
+        scores: [
+            3,
+            4,
+            4,
+            3,
+            3,
+            5,
+            2,
+            4,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Test",
+        photo: "https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
     }
 ];
-  
+
+module.exports = friends;
